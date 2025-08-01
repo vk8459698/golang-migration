604244f3f27f78671083ca82f60a34b568b333c9
vivek <134062734+vk8459698@users.noreply.github.com>
2025-08-01 06:46:42 +0530

    Update ledger.go

--- fetcher-go/src/ledger/ledger.go
+++ fetcher-go/src/ledger/ledger.go
@@ -10,6 +10,8 @@ import (
 	"fetcher-go/src/dex/swaprv2"
 	"fetcher-go/src/dex/traderjoe"
 	"fetcher-go/src/dex/uniswap"
+	"fetcher-go/src/dex/hyperion"
+	"fetcher-go/src/dex/thala"
 	"fmt"
 	"strconv"
 	"sync"
@@ -565,7 +567,171 @@ func (l *Ledger) updatePoolData() error {
 							chainGraph[token1][token0] = true
 							mu.Unlock()
 						}
+					case "HYPERION":
+						pools, err := hyperion.FetchPools(chainId, dexName, dexConfig)
+						if err != nil {
+							fmt.Printf("[Ledger] Error fetching pools for %s: %v\n", dexName, err)
+							return
+						}
 
+						for _, p := range pools {
+							poolMap, ok := p.(map[string]any)
+							if !ok {
+								continue
+							}
+
+							id := common.Checksum(poolMap["poolId"].(string))
+							if id == "0xe79d4ef25f12384f5a541d1e3485df69dd81447f" || id == "0xee9bf1d1e23784067bd7b0b3496f865038b766eb" {
+								continue
+							}
+
+							token0 := common.Checksum(poolMap["token0"].(string))
+							token1 := common.Checksum(poolMap["token1"].(string))
+							symbol0 := poolMap["symbol0"]
+							symbol1 := poolMap["symbol1"]
+
+							decimals0, ok := poolMap["decimals0"].(int)
+							if !ok {
+								continue
+							}
+
+							decimals1, ok := poolMap["decimals1"].(int)
+							if !ok {
+								continue
+							}
+
+							tickCurrent, ok := poolMap["tickCurrent"].(int)
+							if !ok {
+								continue
+							}
+
+							fee, ok := poolMap["fee"].(int)
+							if !ok {
+								continue
+							}
+
+							tokens := common.SortTokens(token0, token1)
+							tokenPairKey := fmt.Sprintf("%s:%s", tokens[0], tokens[1])
+
+							info := map[string]any{
+								"poolId":     id,
+								"dexId":      dexId,
+								"token0":     token0,
+								"token1":     token1,
+								"symbol0":    symbol0,
+								"symbol1":    symbol1,
+								"decimals0":  decimals0,
+								"decimals1":  decimals1,
+								"gasPrice":   "0",
+								"tokenType0": poolMap["tokenType0"],
+								"tokenType1": poolMap["tokenType1"],
+								"fee":        fee,
+								"ticks":      poolMap["ticks"],
+								"sqrtPrice":  poolMap["sqrtPrice"],
+								"tickCurrent": tickCurrent,
+								"liquidity":  poolMap["liquidity"],
+							}
+
+							jsonInfo, _ := json.Marshal(info)
+							redisClient.HSet(ctx, fmt.Sprintf("poolData:%s:%s", chainIdStr, tokenPairKey), id, jsonInfo)
+							redisClient.HSet(ctx, fmt.Sprintf("tokenSymbol:%s", chainIdStr), token0, symbol0)
+							redisClient.HSet(ctx, fmt.Sprintf("tokenSymbol:%s", chainIdStr), token1, symbol1)
+
+							mu.Lock()
+							if _, exists := chainGraph[token0]; !exists {
+								chainGraph[token0] = make(map[string]bool)
+							}
+							if _, exists := chainGraph[token1]; !exists {
+								chainGraph[token1] = make(map[string]bool)
+							}
+							chainGraph[token0][token1] = true
+							chainGraph[token1][token0] = true
+							mu.Unlock()
+						}
+					case "THALA_V2":
+						pools, err := thala.FetchPools(chainId, dexName, dexConfig)
+						if err != nil {
+							fmt.Printf("[Ledger] Error fetching pools for %s: %v\n", dexName, err)
+							return
+						}
+
+						for _, p := range pools {
+							poolMap, ok := p.(map[string]any)
+							if !ok {
+								continue
+							}
+
+							id := common.Checksum(poolMap["poolId"].(string))
+							if id == "0x7845c59627bf2ecd0a8d4e2e83f0008546868442c3027060f042398578213164" {
+								continue
+							}
+
+							token0 := common.Checksum(poolMap["token0"].(string))
+							token1 := common.Checksum(poolMap["token1"].(string))
+							symbol0 := poolMap["symbol0"]
+							symbol1 := poolMap["symbol1"]
+
+							decimals0, ok := poolMap["decimals0"].(int)
+							if !ok {
+								continue
+							}
+
+							decimals1, ok := poolMap["decimals1"].(int)
+							if !ok {
+								continue
+							}
+
+							balances := poolMap["balances"]
+							ampFactor := poolMap["ampFactor"]
+							index0 := poolMap["index0"]
+							index1 := poolMap["index1"]
+							weights := poolMap["weights"]
+							poolType := poolMap["poolType"]
+							fee := poolMap["fee"]
+							tokenType0 := poolMap["tokenType0"]
+							tokenType1 := poolMap["tokenType1"]
+							dexIdNew := poolMap["dexId"].(string)
+
+							tokens := common.SortTokens(token0, token1)
+							tokenPairKey := fmt.Sprintf("%s:%s", tokens[0], tokens[1])
+
+							info := map[string]any{
+								"poolId":     id,
+								"dexId":      dexIdNew,
+								"token0":     token0,
+								"token1":     token1,
+								"symbol0":    symbol0,
+								"symbol1":    symbol1,
+								"decimals0":  decimals0,
+								"decimals1":  decimals1,
+								"tokenType0": tokenType0,
+								"tokenType1": tokenType1,
+								"balances":   balances,
+								"ampFactor":  ampFactor,
+								"index0":     index0,
+								"index1":     index1,
+								"weights":    weights,
+								"poolType":   poolType,
+								"fee":        fee,
+								"gasPrice":   "0",
+							}
+
+							jsonInfo, _ := json.Marshal(info)
+							redisClient.HSet(ctx, fmt.Sprintf("poolData:%s:%s", chainIdStr, tokenPairKey), id, jsonInfo)
+							redisClient.HSet(ctx, fmt.Sprintf("tokenSymbol:%s", chainIdStr), token0, symbol0)
+							redisClient.HSet(ctx, fmt.Sprintf("tokenSymbol:%s", chainIdStr), token1, symbol1)
+
+							mu.Lock()
+							if _, exists := chainGraph[token0]; !exists {
+								chainGraph[token0] = make(map[string]bool)
+							}
+							if _, exists := chainGraph[token1]; !exists {
+								chainGraph[token1] = make(map[string]bool)
+							}
+							chainGraph[token0][token1] = true
+							chainGraph[token1][token0] = true
+							mu.Unlock()
+						}
 					case "TRADERJOE":
 						pools, err := traderjoe.FetchPools(chainId, dexName, dexConfig)
 						if err != nil {
